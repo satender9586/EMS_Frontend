26f75c6784ae777682eb83cfb951b4edeb03729a
satender kumar <skstatenderkumar59@gmail.com>
2025-04-12 14:58:16 +0530

    Api : add new employee api function create

--- src/services/POST_API.tsx
+++ src/services/POST_API.tsx
@@ -1,1 +1,13 @@
+import { instance } from "@/lib/Axis.interceptor";
+const Auth_Base = "/auth"
+
+//  Add_New user API
+
+export const AddNewUserApi = async () => {
+    const response = await instance.post(`${Auth_Base}/register`);
+    if (response.status !== 200) {
+        throw new Error("Something is wrong!");
+    }
+    return response;
+};
 
